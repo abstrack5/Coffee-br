d49988a2ba32cc98fdbb6437e2d8f0c3e96a053f
Abstrack <Armon_ah05@outlook.com>
2022-06-15 14:59:38 -0400

    added rest of controllers, edit models, setup index model

--- models/index.js
+++ models/index.js
@@ -1,1 +1,67 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Likes = require('./Likes');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.belongsToMany(Post, {
+    through: Likes,
+    as: 'liked_posts',
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Likes,
+    as: 'liked_posts',
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Likes.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Likes.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Likes, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Likes, {
+    foreignKey: 'post_id'
+  });
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+  
+  module.exports = { User, Post, Likes, Comment };
