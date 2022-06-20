ec216c3af391a801c16828de5476ad111605a01d
Abstrack <Armon_ah05@outlook.com>
2022-06-20 06:21:06 -0400

    Fixed first error not loading page properly. Added signup button to login, and login to signup. added app name to login screen. NEED TO FIX ERROR IN CONSOLE

---
+++ controllers/home-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { request, response } = require("express");
+const sequelize = require("../config/connection");
+const { Coffee, Dairy, Employee, Flavor, Order, Sweetener } = require("../models");
+
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,26 +7,26 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 
-// const hbs = exphbs.create({});
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//     secret: process.env.SECRET,
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// }
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
-// app.use(session(sess));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
