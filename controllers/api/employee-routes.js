2e30fe8918cc611f62bcf21be768e8823767fbc8
egemenelz <egemenelz@gmail.com>
2022-06-21 00:30:21 -0400

    added styling to login page

--- controllers/api/employee-routes.js
+++ controllers/api/employee-routes.js
@@ -50,13 +50,28 @@ router.get("/:id", (req, res) => {
     });
 });
 
-router.post("/",  (req, res) => {
+router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   Employee.create({
     username: req.body.username,
     password: req.body.password,
     pin: req.body.pin,
-  }).then((dbEmployeeData) => res.json(dbEmployeeData));
+  })
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.password = dbUserData.password;
+        req.session.pin = dbUserData.pin;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put("/:id", (req, res) => {
