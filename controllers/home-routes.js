644a11283312865b75603678ea73435ea11d276c
Abstrack <Armon_ah05@outlook.com>
2022-06-21 01:23:24 -0400

    content added broke app again, login and signup still doesnt work. Commiting out home-routes order.findall lets login screen load again

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,44 +2,44 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Coffee, Dairy, Employee, Flavor, Order, Sweetener } = require("../models");
 
-router.get("/", (req, res) => {
-  Order.findAll({
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
+// router.get("/", (req, res) => {
+//   Order.findAll({
+//     attributes: [
+//       "id",
+//       "post_url",
+//       "title",
+//       "created_at",
       
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      // pass a single post object into the homepage template
-      res.render("homepage", {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       const posts = dbPostData.map((post) => post.get({ plain: true }));
+//       // pass a single post object into the homepage template
+//       res.render("homepage", {
+//         posts,
+//         loggedIn: req.session.loggedIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
 
-  });
+//   });
 
 router.get("/", (req, res) => {
   res.render("login");
